b3ee3807502f77a2565333b13c3c632501750bc0
Luz Gutierrez <mili01gm@gmail.com>
2017-05-02 00:47:01 -0500

    campo edad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
-var edadFragment = [];
-function creaEdad(){
-  
+var regiones = ["Amazonas","Áncash","Apurímac","Arequipa","Ayacucho","Cajamarca","Callao","Cuzco","Huancavelica","Huánuco","Ica","Junin","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua","Pasco","Piura","Puno","San Martin","Tacna","Tumbes","Ucayali"];
+var edad = []
+  for (var i=1; i<100;i++){
+    edad.push(i);
+  }
+
+function newOption(elem){
+  var option = document.createElement("option");
+  option.setAttribute("value",elem);
+  option.innerText = elem;
+  return option;
 }
 
-document.getElementById("edad").appendChild(edadFragment);
+
+window.addEventListener("load",function(){
+  var sectionEdad = edad.forEach(function(e){
+    var option = document.createElement("option");
+    option.setAttribute("value","0");
+    option.hidden = "";
+    document.getElementById("edad").appendChild(option);
+    document.getElementById("edad").appendChild(newOption(e));
+  });
+  return sectionEdad;
+})
